930265726e3fb6a1d5783dacb9d16c00c90dbecc
BT Templeton <bpt@hcoop.net>
2012-05-30 12:22:35 -0400

    remove asynchronous input processing
    * configure.in (SYNC_INPUT): Remove this macro definition and its
      associated configure option. All references changed to assume that
      SYNC_INPUT is enabled.
    * src/alloc.c (_bytes_used, __malloc_extra_blocks, _malloc_internal)
      (_free_internal, malloc_hysteresis): Remove variables which aren't
      used when SYNC_INPUT is enabled.
    
    Conflicts:
    
    	src/alloc.c

--- src/alloc.c
+++ src/alloc.c
@@ -723,45 +723,6 @@ static_assert (LISP_ALIGNMENT % GCALIGNMENT == 0);
 enum { MALLOC_IS_LISP_ALIGNED = alignof (max_align_t) % LISP_ALIGNMENT == 0 };
 static_assert (MALLOC_IS_LISP_ALIGNED);
 
-/* If compiled with XMALLOC_BLOCK_INPUT_CHECK, define a symbol
-   BLOCK_INPUT_IN_MEMORY_ALLOCATORS that is visible to the debugger.
-   If that variable is set, block input while in one of Emacs's memory
-   allocation functions.  There should be no need for this debugging
-   option, since signal handlers do not allocate memory, but Emacs
-   formerly allocated memory in signal handlers and this compile-time
-   option remains as a way to help debug the issue should it rear its
-   ugly head again.  */
-#ifdef XMALLOC_BLOCK_INPUT_CHECK
-bool block_input_in_memory_allocators EXTERNALLY_VISIBLE;
-static void
-malloc_block_input (void)
-{
-  if (block_input_in_memory_allocators)
-    block_input ();
-}
-static void
-malloc_unblock_input (void)
-{
-  if (block_input_in_memory_allocators)
-    {
-      int err = errno;
-      unblock_input ();
-      errno = err;
-    }
-}
-# define MALLOC_BLOCK_INPUT malloc_block_input ()
-# define MALLOC_UNBLOCK_INPUT malloc_unblock_input ()
-#else
-# define MALLOC_BLOCK_INPUT ((void) 0)
-# define MALLOC_UNBLOCK_INPUT ((void) 0)
-#endif
-
-#define MALLOC_PROBE(size)			\
-  do {						\
-    if (profiler_memory_running)		\
-      malloc_probe (size);			\
-  } while (0)
-
 /* Like malloc but check for no memory and block interrupt input.  */
 
 void *
@@ -769,13 +730,10 @@ xmalloc (size_t size)
 {
   void *val;
 
-  MALLOC_BLOCK_INPUT;
   val = malloc (size);
-  MALLOC_UNBLOCK_INPUT;
 
   if (!val)
     memory_full (size);
-  MALLOC_PROBE (size);
   return val;
 }
 
@@ -786,13 +744,11 @@ xzalloc (size_t size)
 {
   void *val;
 
-  MALLOC_BLOCK_INPUT;
-  val = calloc (1, size);
-  MALLOC_UNBLOCK_INPUT;
+  val = malloc (size);
 
   if (!val)
     memory_full (size);
-  MALLOC_PROBE (size);
+  memset (val, 0, size);
   return val;
 }
 
@@ -803,13 +759,10 @@ xrealloc (void *block, size_t size)
 {
   void *val;
 
-  MALLOC_BLOCK_INPUT;
   val = realloc (block, size);
-  MALLOC_UNBLOCK_INPUT;
 
   if (!val)
     memory_full (size);
-  MALLOC_PROBE (size);
   return val;
 }
 
@@ -823,9 +776,8 @@ xfree (void *block)
     return;
   if (pdumper_object_p (block))
     return;
-  MALLOC_BLOCK_INPUT;
   free (block);
-  MALLOC_UNBLOCK_INPUT;
+
   /* We don't call refill_memory_reserve here
      because in practice the call in r_alloc_free seems to suffice.  */
 }
@@ -1011,8 +963,6 @@ lisp_malloc (size_t nbytes, bool clearit, enum mem_type type)
 {
   register void *val;
 
-  MALLOC_BLOCK_INPUT;
-
 #ifdef GC_MALLOC_CHECK
   allocated_mem_type = type;
 #endif
@@ -1041,10 +991,8 @@ lisp_malloc (size_t nbytes, bool clearit, enum mem_type type)
     mem_insert (val, (char *) val + nbytes, type);
 #endif
 
-  MALLOC_UNBLOCK_INPUT;
   if (!val)
     memory_full (nbytes);
-  MALLOC_PROBE (nbytes);
   return val;
 }
 
@@ -1057,7 +1005,6 @@ lisp_free (void *block)
   if (pdumper_object_p (block))
     return;
 
-  MALLOC_BLOCK_INPUT;
 #ifndef GC_MALLOC_CHECK
   struct mem_node *m = mem_find (block);
 #endif
@@ -1065,7 +1012,6 @@ lisp_free (void *block)
 #ifndef GC_MALLOC_CHECK
   mem_delete (m);
 #endif
-  MALLOC_UNBLOCK_INPUT;
 }
 
 /*****  Allocation of aligned blocks of memory to store Lisp data.  *****/
@@ -1203,8 +1149,6 @@ lisp_align_malloc (size_t nbytes, enum mem_type type)
 
   eassert (nbytes <= BLOCK_BYTES);
 
-  MALLOC_BLOCK_INPUT;
-
 #ifdef GC_MALLOC_CHECK
   allocated_mem_type = type;
 #endif
@@ -1228,10 +1172,7 @@ lisp_align_malloc (size_t nbytes, enum mem_type type)
 #endif
 
       if (base == 0)
-	{
-	  MALLOC_UNBLOCK_INPUT;
-	  memory_full (ABLOCKS_BYTES);
-	}
+        memory_full (ABLOCKS_BYTES);
 
       aligned = (base == abase);
       if (!aligned)
@@ -1255,7 +1196,6 @@ lisp_align_malloc (size_t nbytes, enum mem_type type)
 	    {
 	      lisp_malloc_loser = base;
 	      free (base);
-	      MALLOC_UNBLOCK_INPUT;
 	      memory_full (SIZE_MAX);
 	    }
 	}
@@ -1292,10 +1232,6 @@ lisp_align_malloc (size_t nbytes, enum mem_type type)
     mem_insert (val, (char *) val + nbytes, type);
 #endif
 
-  MALLOC_UNBLOCK_INPUT;
-
-  MALLOC_PROBE (nbytes);
-
   eassert (0 == ((uintptr_t) val) % BLOCK_ALIGN);
   return val;
 }
@@ -1306,10 +1242,10 @@ lisp_align_free (void *block)
   struct ablock *ablock = block;
   struct ablocks *abase = ABLOCK_ABASE (ablock);
 
-  MALLOC_BLOCK_INPUT;
 #ifndef GC_MALLOC_CHECK
   mem_delete (mem_find (block));
 #endif
+
   /* Put on free list.  */
   ablock->x.next_free = free_ablock;
   ASAN_POISON_ABLOCK (ablock);
@@ -1346,9 +1282,7 @@ lisp_align_free (void *block)
 #endif
       free (ABLOCKS_BASE (abase));
     }
-  MALLOC_UNBLOCK_INPUT;
 }
-
 
 /***********************************************************************
 			 Interval Allocation
@@ -1409,8 +1343,6 @@ make_interval (void)
 {
   INTERVAL val;
 
-  MALLOC_BLOCK_INPUT;
-
   if (interval_free_list)
     {
       val = interval_free_list;
@@ -1432,8 +1364,6 @@ make_interval (void)
       val = &interval_block->intervals[interval_block_index++];
       ASAN_UNPOISON_INTERVAL (val);
     }
-
-  MALLOC_UNBLOCK_INPUT;
 
   tally_consing (sizeof (struct interval));
   intervals_consed++;
@@ -1804,8 +1734,6 @@ allocate_string (void)
 {
   struct Lisp_String *s;
 
-  MALLOC_BLOCK_INPUT;
-
   /* If the free-list is empty, allocate a new string_block, and
      add all the Lisp_Strings in it to the free-list.  */
   if (string_free_list == NULL)
@@ -1833,8 +1761,6 @@ allocate_string (void)
   s = string_free_list;
   ASAN_UNPOISON_STRING (s);
   string_free_list = NEXT_FREE_LISP_STRING (s);
-
-  MALLOC_UNBLOCK_INPUT;
 
   ++strings_consed;
   tally_consing (sizeof *s);
@@ -1878,8 +1804,6 @@ allocate_string_data (struct Lisp_String *s,
   /* Determine the number of bytes needed to store NBYTES bytes
      of string data.  */
   ptrdiff_t needed = sdata_size (nbytes);
-
-  MALLOC_BLOCK_INPUT;
 
   if (nbytes > LARGE_STRING_BYTES || immovable)
     {
@@ -1943,8 +1867,6 @@ allocate_string_data (struct Lisp_String *s,
   data->string = s;
   b->next_free = (sdata *) ((char *) data + needed + GC_STRING_EXTRA);
   eassert ((uintptr_t) b->next_free % alignof (sdata) == 0);
-
-  MALLOC_UNBLOCK_INPUT;
 
   s->u.s.data = SDATA_DATA (data);
 #ifdef GC_CHECK_STRING_BYTES
@@ -2671,8 +2593,6 @@ make_float (double float_value)
 {
   register Lisp_Object val;
 
-  MALLOC_BLOCK_INPUT;
-
   if (float_free_list)
     {
       XSETFLOAT (val, float_free_list);
@@ -2695,8 +2615,6 @@ make_float (double float_value)
       XSETFLOAT (val, &float_block->floats[float_block_index]);
       float_block_index++;
     }
-
-  MALLOC_UNBLOCK_INPUT;
 
   XFLOAT_INIT (val, float_value);
   eassert (!XFLOAT_MARKED_P (XFLOAT (val)));
@@ -2795,8 +2713,6 @@ DEFUN ("cons", Fcons, Scons, 2, 2, 0,
 {
   register Lisp_Object val;
 
-  MALLOC_BLOCK_INPUT;
-
   if (cons_free_list)
     {
       ASAN_UNPOISON_CONS (cons_free_list);
@@ -2819,8 +2735,6 @@ DEFUN ("cons", Fcons, Scons, 2, 2, 0,
       XSETCONS (val, &cons_block->conses[cons_block_index]);
       cons_block_index++;
     }
-
-  MALLOC_UNBLOCK_INPUT;
 
   XSETCAR (val, car);
   XSETCDR (val, cdr);
@@ -3890,8 +3804,6 @@ Its value is void, and its function definition and property list are nil.  */)
 
   CHECK_STRING (name);
 
-  MALLOC_BLOCK_INPUT;
-
   if (symbol_free_list)
     {
       ASAN_UNPOISON_SYMBOL (symbol_free_list);
@@ -3914,8 +3826,6 @@ Its value is void, and its function definition and property list are nil.  */)
       XSETSYMBOL (val, &symbol_block->symbols[symbol_block_index]);
       symbol_block_index++;
     }
-
-  MALLOC_UNBLOCK_INPUT;
 
   init_symbol (val, name);
   tally_consing (sizeof (struct Lisp_Symbol));
@@ -4325,14 +4235,12 @@ memory_full (size_t nbytes)
     {
       void *p;
 
-      MALLOC_BLOCK_INPUT;
       p = malloc (SPARE_MEMORY);
       if (p)
 	{
 	  free (p);
 	  enough_free_memory = true;
 	}
-      MALLOC_UNBLOCK_INPUT;
     }
 
   if (! enough_free_memory)
