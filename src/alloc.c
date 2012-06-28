d848c3629d160bc9b15ae24f27a05aa511380e3e
BT Templeton <bpt@hcoop.net>
2012-06-27 21:22:59 -0400

    use xmalloc_unsafe in memory_full
    * src/alloc.c (memory_full): Use `xmalloc_unsafe'.

--- src/alloc.c
+++ src/alloc.c
@@ -1825,7 +1825,7 @@ memory_full (size_t nbytes)
   bool enough_free_memory = false;
   if (SPARE_MEMORY < nbytes)
     {
-      void *p = GC_MALLOC (SPARE_MEMORY);
+      void *p = xmalloc_unsafe (SPARE_MEMORY);
       if (p)
 	{
 	  xfree (p);
