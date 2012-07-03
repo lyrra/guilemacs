ac2555bcb806569fd54b8b11b8ad5e289df606a8
BT Templeton <bpt@hcoop.net>
2012-07-03 17:53:22 -0400

    initialize guile
    * src/emacs.c (main): Call `scm_init_guile'.

--- src/emacs.c
+++ src/emacs.c
@@ -27,6 +27,7 @@ along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #include <sys/file.h>
 #include <unistd.h>
+#include <libguile.h>
 
 #include <close-stream.h>
 
@@ -766,6 +767,10 @@ main (int argc, char **argv)
 
   init_standard_fds ();
   atexit (close_output_streams);
+
+  /* Override Guile's libgc configuration. */
+  xputenv ("GC_ALL_INTERIOR_POINTERS=1");
+  scm_init_guile ();
 
   sort_args (argc, argv);
   argc = 0;
