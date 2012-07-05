8a1322d20ccf233a30cd51974eabce9bbe93ca36
BT Templeton <bpt@hcoop.net>
2012-07-04 20:53:46 -0400

    don't create a hash table for pure space
    * lisp/loadup.el: Leave purify-flag as `t' instead of allocating a hash
      table for hash-consing.

--- lisp/loadup.el
+++ lisp/loadup.el
@@ -105,10 +105,6 @@
       ;; than usual.
       (setq max-lisp-eval-depth (max max-lisp-eval-depth 3400))))
 
-(if (eq t purify-flag)
-    ;; Hash consing saved around 11% of pure space in my tests.
-    (setq purify-flag (make-hash-table :test #'equal :size 80000)))
-
 (message "Using load-path %s" load-path)
 
 (if dump-mode
