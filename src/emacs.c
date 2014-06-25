66d3fcfa8f1930fbc9b2b0a8b397ac60a64e2e83
Robin Templeton <robin@terpri.org>
2014-06-25 03:10:37 -0400

    * src/profiler.c: Delete.

--- src/emacs.c
+++ src/emacs.c
@@ -2298,7 +2298,6 @@ Using an Emacs configured with --with-x-toolkit=lucid does not have this problem
 
       syms_of_xwidget ();
       syms_of_threads ();
-      syms_of_profiler ();
       syms_of_pdumper ();
 
 #ifdef HAVE_JSON
