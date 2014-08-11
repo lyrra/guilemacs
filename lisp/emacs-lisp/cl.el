d6b3357008eb7e27c3322b73bda7cd4f0b75d93a
Robin Templeton <robin@terpri.org>
2014-08-11 06:59:28 -0400

    guile-elisp eval-when

--- lisp/emacs-lisp/cl.el
+++ lisp/emacs-lisp/cl.el
@@ -247,7 +247,6 @@
                ecase
                case
                load-time-value
-               eval-when
                destructuring-bind
                gentemp
                pairlis
