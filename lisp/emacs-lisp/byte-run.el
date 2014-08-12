0d1ff1381fa59c544ef7c117f15edeb75ee1cf53
Robin Templeton <robin@terpri.org>
2014-08-12 07:41:14 -0400

    defun/defmacro autoload generation

--- lisp/emacs-lisp/byte-run.el
+++ lisp/emacs-lisp/byte-run.el
@@ -458,15 +458,16 @@ The return value is undefined.
                      (list 'function
                            (cons 'lambda
                                  (cons arglist body))))))
-      (list 'prog1
-            (if declarations
-                (cons 'prog1 (cons def (car declarations)))
-              def)
+      (list 'progn
+            def
+            (cons 'progn (car declarations))
+            :autoload-end
             (list 'funcall
                   (list '@ '(guile) 'set-procedure-property!)
                   (list 'symbol-function (list 'quote name))
                   (list 'quote 'name)
-                  (list 'quote name))))))
+                  (list 'quote name))
+            (list 'quote name)))))
 
 ;; Redefined in byte-opt.el.
 ;; This was undocumented and unused for decades.
