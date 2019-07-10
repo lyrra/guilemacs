45b624d1d17043c094bfb91eba93b432b8ac9e24
larry <larryvalkama@protonmail.com>
2019-07-10 22:56:59 +0200

    Make syms_of_xdisp load before syms_of_textprop
    syms_of_textprop DEFVARs a special variable and
    initializes it to a form with Qdisplay which doesn't exist
    until syms_of_xdisp is loaded.

--- src/emacs.c
+++ src/emacs.c
@@ -1962,6 +1962,7 @@ Using an Emacs configured with --with-x-toolkit=lucid does not have this problem
 #ifdef HAVE_SOUND
       syms_of_sound ();
 #endif
+      syms_of_xdisp();
       syms_of_textprop ();
       syms_of_composite ();
 #ifdef WINDOWSNT
@@ -1971,7 +1972,6 @@ Using an Emacs configured with --with-x-toolkit=lucid does not have this problem
       syms_of_cygw32 ();
 #endif
       syms_of_window ();
-      syms_of_xdisp ();
       syms_of_font ();
 #ifdef HAVE_WINDOW_SYSTEM
       syms_of_fringe ();
