a0b16c43fc698e5c811c8e3479d5b53bab47f9ff
larry <gudrun>
2023-02-21 08:20:37 +0100

    FIX: unbind_to usage might be warranted?

--- src/fns.c
+++ src/fns.c
@@ -5061,8 +5061,8 @@ extract_data_from_object (Lisp_Object spec,
     {
       struct buffer *prev = current_buffer;
       EMACS_INT b, e;
-      ptrdiff_t count = SPECPDL_INDEX ();
-      dynwind_begin ();
+      //ptrdiff_t count = SPECPDL_INDEX ();
+      //dynwind_begin ();
 
       record_unwind_current_buffer ();
 
@@ -5147,8 +5147,8 @@ extract_data_from_object (Lisp_Object spec,
       /* Discard the unwind protect for recovering the current
 	 buffer.  */
       specpdl_ptr--;
-      unbind_to (count, Qnil);
-      dynwind_end ();
+      //unbind_to (count, Qnil); //FIX-20230221-LAV: possible warranted use of unbind_to
+      //dynwind_end ();
 
       if (STRING_MULTIBYTE (object))
 	object = code_convert_string (object, coding_system,
