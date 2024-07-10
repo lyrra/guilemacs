645353c613947a2a8e7189f245fc6450cf9d1976
Larry Valkama <lz64@proton.me>
2024-07-10 13:33:22 +0200

    fix keyboard.c declared_special

--- src/keyboard.c
+++ src/keyboard.c
@@ -13355,7 +13355,6 @@ for that character after that prefix key.  */);
 	       doc: /* Form to evaluate when Emacs starts up.
 Useful to set before you dump a modified Emacs.  */);
   Vtop_level = Qnil;
-  XSYMBOL (Qtop_level)->u.s.declared_special = false;
 
   DEFVAR_KBOARD ("keyboard-translate-table", Vkeyboard_translate_table,
                  doc: /* Translate table for local keyboard input, or nil.
