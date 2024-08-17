cea4b81754be1eb21a986f272bdfbbd1e345a93c
Larry Valkama <lz64@proton.me>
2024-08-17 15:14:41 +0200

    fix setf-place over push-place (avoids pcase)
    fixes f216f388, that moved read-char-from-minibuffer to subr where
    pcase hasn't been loaded

--- lisp/subr.el
+++ lisp/subr.el
@@ -3744,7 +3744,9 @@ There is no need to explicitly add `help-char' to CHARS;
               ;; We have a string (with one character), so return the first one.
               (elt result 0)
             ;; The default value is RET.
-            (when history (push "\r" (symbol-value history)))
+            (when history
+              (setf (symbol-value history)
+                    (cons "\r" (symbol-value history))))
             ?\r)))
     ;; Display the question with the answer.
     (message "%s%s" prompt (char-to-string char))
