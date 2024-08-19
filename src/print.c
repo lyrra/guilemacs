5cfde1607e7dba399e9d82e4a08c278ca2c7784f
Larry Valkama <lz64@proton.me>
2024-08-19 11:45:57 +0200

    dont call strout with size_byte=-1

--- src/print.c
+++ src/print.c
@@ -2556,8 +2556,9 @@ print_object (Lisp_Object obj, Lisp_Object printcharfun, bool escapeflag)
         else
           scm_write (obj, port);
         scm_display (SCM_MAKE_CHAR ('>'), port);
-        strout (scm_to_locale_string (scm_get_output_string (port)),
-                -1, -1, printcharfun);
+        //guilemacs: this looks wrong (size_byte = -1)
+        //strout (scm_to_locale_string (scm_get_output_string (port)),
+        //        -1, -1, printcharfun);
         scm_close_port (port);
       }
       break;
