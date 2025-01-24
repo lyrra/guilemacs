b615221c5db7c9fa49c05abd146accf5709844cf
Larry Valkama <lz64@proton.me>
2025-01-24 11:55:11 +0100

    prelude args handling
    avoid the convoluted system emacs uses and search argv directly

--- src/emacs.c
+++ src/emacs.c
@@ -290,7 +290,7 @@ Initialization options:\n\
 --no-build-details          do not add build details such as time stamps\n\
 --no-desktop                do not load a saved desktop\n\
 --no-init-file, -q          load neither ~/.emacs nor default.el\n\
---prelude, -prelude         load file instead of loadup.el into bare Emacs\n\
+--prelude                   load file instead of loadup.el into bare Emacs\n\
 --no-loadup, -nl            do not load loadup.el into bare Emacs\n\
 --no-site-file              do not load site-start.el\n\
 --no-x-resources            do not load X resources\n\
@@ -2272,16 +2272,17 @@ Using an Emacs configured with --with-x-toolkit=lucid does not have this problem
         {
 	  Vtop_level = list2 (Qload, build_string ("loadup.el"));
         } else {
-          while (skip_args < argc)
+          Lisp_Object loads = Qnil;
+          for (int i = 0; i < argc - 1; i++)
             {
-              argmatch (argv, argc, "-prelude", "--prelude", 7, &prelude, &skip_args);
-
-              if (prelude)
+              if (!(strcmp(argv[i], "--prelude")))
                 {
-                  Vtop_level = list2 (Qload, build_unibyte_string (prelude));
-                  break;
+                  loads = Fcons (list2 (Qload, build_unibyte_string (argv[i + 1])), loads);
                 }
-              skip_args++;
+            }
+          if (!NILP (loads))
+            {
+              Vtop_level = Fcons (Qprogn, loads);
             }
         }
 
