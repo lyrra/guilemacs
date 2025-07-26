a6f8e44057c220a284e804d163a08c026dd58e5c
Larry Valkama <lz64@proton.me>
2025-07-26 10:19:47 +0200

    4/ test pre second-refactor-effect compare-strings-ns

--- test/pre/string2.scm
+++ test/pre/string2.scm
@@ -6,20 +6,31 @@
 (deftest compare-strings-ss (t)
   (el-expr `(print (compare-strings "\"Test\"" nil nil "\"test\"" nil nil t))))
 
-(let ((emit-test (lambda (s e)
-                   (deftestf 'compare-strings-ns (e)
-                     (el-expr `(print (compare-strings ,s    ; str1
-                                                       ; start1 -- defaults to 0
-                                                       ,(if (= 0 (random 2)) 'nil 0)
-                                                       ; end1   -- defaults to length of string
-                                                       ,(if (= 0 (random 2)) 'nil 1)
+(let ((emit-test (lambda (t1 t2 ignore-case e)
+                   (match (list t1 t2)
+                     (((str1 start1 end1) (str2 start2 end2))
+                      (deftestf 'compare-strings-ns (e)
+                        (el-expr `(print (compare-strings
+                                           ,str1
+                                           ,start1 ; defaults to 0
+                                           ,end1 ; defaults to length of string
 
-                                                       ,s    ; str2
-                                                       ; start2 -- defaults to 0
-                                                       ,(if (= 0 (random 2)) 'nil 0)
-                                                       ; end2   -- defaults to length of string
-                                                       ,(if (= 0 (random 2)) 'nil 1)
+                                           ,str2
+                                           ,start2 ; defaults to 0
+                                           ,end2 ; defaults to length of string
 
-                                                       ; ignore_case
-                                                       nil)))))))
-  (emit-test "\"a\"" 't))
+                                           ,ignore-case)))))))))
+  (let ((s "\"a\"")
+        (l 1))
+    (emit-test ; first string triplet :: (string start end)
+               (list s
+                     (if (= 0 (random 2)) 'nil 0)
+                     (if (= 0 (random 2)) 'nil l))
+               ; second string triplet
+               (list s
+                     (if (= 0 (random 2)) 'nil 0)
+                     (if (= 0 (random 2)) 'nil l))
+               ; ignore-case
+               (if (= 0 (random 2)) 'nil 't)
+               ; expected result
+               't)))
