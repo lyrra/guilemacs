ae9b371a2f19409ad4bd05babd46f58fc2441ad4
Larry Valkama <lz64@proton.me>
2025-07-26 09:24:18 +0200

    1/ test pre fix compare-strings-ns

--- test/pre/string2.scm
+++ test/pre/string2.scm
@@ -6,5 +6,13 @@
 (deftest compare-strings-ss (t)
   (el-expr `(print (compare-strings "\"Test\"" nil nil "\"test\"" nil nil t))))
 
-(deftest compare-strings-ns (t)
-  (el-expr `(print (compare-strings nil 0 nil "\"a\"" 0 nil nil))))
+(let ((s "\"a\""))
+  (deftestf 'compare-strings-ns ('t)
+    (el-expr `(print (compare-strings ,s    ; str1
+                                      0     ; start1 -- defaults to 0
+                                      nil   ; end1   -- defaults to length of string
+                                      ,s    ; str2
+                                      0     ; start2 -- defaults to 0
+                                      nil   ; end2   -- defaults to length of string
+                                      nil   ; ignore_case
+                                      )))))
