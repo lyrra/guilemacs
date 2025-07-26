b38c2030bb044ce16f0e0cbd3b2f89b0f90a2079
Larry Valkama <lz64@proton.me>
2025-07-26 09:35:04 +0200

    2/ test pre randomize compare-strings-ns

--- test/pre/string2.scm
+++ test/pre/string2.scm
@@ -9,10 +9,16 @@
 (let ((s "\"a\""))
   (deftestf 'compare-strings-ns ('t)
     (el-expr `(print (compare-strings ,s    ; str1
-                                      0     ; start1 -- defaults to 0
-                                      nil   ; end1   -- defaults to length of string
+                                      ; start1 -- defaults to 0
+                                      ,(if (= 0 (random 2)) 'nil 0)
+                                      ; end1   -- defaults to length of string
+                                      ,(if (= 0 (random 2)) 'nil 1)
+
                                       ,s    ; str2
-                                      0     ; start2 -- defaults to 0
-                                      nil   ; end2   -- defaults to length of string
-                                      nil   ; ignore_case
-                                      )))))
+                                      ; start2 -- defaults to 0
+                                      ,(if (= 0 (random 2)) 'nil 0)
+                                      ; end2   -- defaults to length of string
+                                      ,(if (= 0 (random 2)) 'nil 1)
+
+                                      ; ignore_case
+                                      nil)))))
