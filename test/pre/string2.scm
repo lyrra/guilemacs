91327713a74464f3212c35fa1861b3c4551c3de0
Larry Valkama <lz64@proton.me>
2025-07-26 09:41:15 +0200

    3/ test pre refactor compare-strings-ns

--- test/pre/string2.scm
+++ test/pre/string2.scm
@@ -6,19 +6,20 @@
 (deftest compare-strings-ss (t)
   (el-expr `(print (compare-strings "\"Test\"" nil nil "\"test\"" nil nil t))))
 
-(let ((s "\"a\""))
-  (deftestf 'compare-strings-ns ('t)
-    (el-expr `(print (compare-strings ,s    ; str1
-                                      ; start1 -- defaults to 0
-                                      ,(if (= 0 (random 2)) 'nil 0)
-                                      ; end1   -- defaults to length of string
-                                      ,(if (= 0 (random 2)) 'nil 1)
+(let ((emit-test (lambda (s e)
+                   (deftestf 'compare-strings-ns (e)
+                     (el-expr `(print (compare-strings ,s    ; str1
+                                                       ; start1 -- defaults to 0
+                                                       ,(if (= 0 (random 2)) 'nil 0)
+                                                       ; end1   -- defaults to length of string
+                                                       ,(if (= 0 (random 2)) 'nil 1)
 
-                                      ,s    ; str2
-                                      ; start2 -- defaults to 0
-                                      ,(if (= 0 (random 2)) 'nil 0)
-                                      ; end2   -- defaults to length of string
-                                      ,(if (= 0 (random 2)) 'nil 1)
+                                                       ,s    ; str2
+                                                       ; start2 -- defaults to 0
+                                                       ,(if (= 0 (random 2)) 'nil 0)
+                                                       ; end2   -- defaults to length of string
+                                                       ,(if (= 0 (random 2)) 'nil 1)
 
-                                      ; ignore_case
-                                      nil)))))
+                                                       ; ignore_case
+                                                       nil)))))))
+  (emit-test "\"a\"" 't))
