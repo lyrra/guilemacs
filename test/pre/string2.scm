073c36f88ce6078b6b2c68d93f9465833edd25e3
Larry Valkama <lz64@proton.me>
2025-07-26 10:55:22 +0200

    5/ test pre compare-strings-ns reap refactor

--- test/pre/string2.scm
+++ test/pre/string2.scm
@@ -3,13 +3,10 @@
   (el-expr `(let ((x "\"cba\""))
               (print (reverse x)))))
 
-(deftest compare-strings-ss (t)
-  (el-expr `(print (compare-strings "\"Test\"" nil nil "\"test\"" nil nil t))))
-
-(let ((emit-test (lambda (t1 t2 ignore-case e)
+(let ((emit-test (lambda (name t1 t2 ignore-case e)
                    (match (list t1 t2)
                      (((str1 start1 end1) (str2 start2 end2))
-                      (deftestf 'compare-strings-ns (e)
+                      (deftestf name (e)
                         (el-expr `(print (compare-strings
                                            ,str1
                                            ,start1 ; defaults to 0
@@ -20,17 +17,24 @@
                                            ,end2 ; defaults to length of string
 
                                            ,ignore-case)))))))))
-  (let ((s "\"a\"")
-        (l 1))
-    (emit-test ; first string triplet :: (string start end)
-               (list s
-                     (if (= 0 (random 2)) 'nil 0)
-                     (if (= 0 (random 2)) 'nil l))
-               ; second string triplet
-               (list s
-                     (if (= 0 (random 2)) 'nil 0)
-                     (if (= 0 (random 2)) 'nil l))
-               ; ignore-case
-               (if (= 0 (random 2)) 'nil 't)
-               ; expected result
-               't)))
+  (for-each (lambda (lst)
+              (match lst
+                ((name (str len))
+                 (emit-test
+                  name
+                  ; first string triplet :: (string start end)
+                  (list str
+                        (if (= 0 (random 2)) 'nil 0)
+                        (if (= 0 (random 2)) 'nil len))
+                  ; second string triplet
+                  (list str
+                        (if (= 0 (random 2)) 'nil 0)
+                        (if (= 0 (random 2)) 'nil len))
+                  ; ignore-case
+                  (if (= 0 (random 2)) 'nil 't)
+                  ; expected result
+                  't))))
+   ; we need to pass the length, if calculated it would count the escaped chars
+   ; though we can't use the length to test substring matching (FIX:)
+   '((compare-strings-1 ("\"a\"" 1))
+     (compare-strings-2 ("\"Test\"" 4)))))
