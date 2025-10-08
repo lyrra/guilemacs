2e9d202bc0409ff21a7e2e796b7fb8bd4aff7d9d
Larry Valkama <lz64@proton.me>
2025-10-08 22:55:41 +0200

    load path url

--- prelude/load.scm
+++ prelude/load.scm
@@ -4372,6 +4372,7 @@ Returns: (new-loads-in-progress . (lexical-binding . (found-eff . hist-file-name
 (set! %load-path (cons "../lisp/vc" %load-path))
 (set! %load-path (cons "./lisp/vc" %load-path))
 (set! %load-path (cons "../lisp/mail" %load-path))
+(set! %load-path (cons "../lisp/url" %load-path))
 
 (set! %load-extensions (cons ".el" %load-extensions))
 
