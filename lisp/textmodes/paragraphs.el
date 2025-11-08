c6367aec44106907563ad88b3d354a5260c51dfc
Larry Valkama <lz64@proton.me>
2025-11-08 08:30:18 +0100

    (sq) fix sentence-end
    incorrectly simplified to return just "." instead of building the proper regex pattern.
    Restored the complete implementation of sentence-end() that constructs the regex from:
      - sentence-end-base
      - sentence-end-double-space
      - sentence-end-without-period
      - sentence-end-without-space
    
    also add a check to handle empty sentence-end-without-space string to avoid creating invalid regex []

--- lisp/textmodes/paragraphs.el
+++ lisp/textmodes/paragraphs.el
@@ -201,8 +201,17 @@ must be followed by two spaces, with perhaps some closing delimiters
 in between.  See Info node `(elisp)Standard Regexps'."
   (or sentence-end
       ;; We accept non-break space along with space.
-      "."
-      ))
+      (concat (if sentence-end-without-period "\\w[ \u00a0][ \u00a0]\\|")
+	      "\\("
+	      sentence-end-base
+              (if sentence-end-double-space
+                  "\\($\\|[ \u00a0]$\\|\t\\|[ \u00a0][ \u00a0]\\)" "\\($\\|[\t \u00a0]\\)")
+              (if (and sentence-end-without-space
+                       (not (string-equal sentence-end-without-space "")))
+                  (concat "\\|[" sentence-end-without-space "]+")
+                "")
+	      "\\)"
+              "[ \u00a0\t\n]*")))
 
 (defcustom page-delimiter "^\\014"
   "Regexp describing line-beginnings that separate pages."
