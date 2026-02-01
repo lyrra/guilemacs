271573369b3fef68a2efe21e6d5768907c58d0ee
Larry Valkama <lz64@proton.me>
2026-02-01 08:36:39 +0100

    inline list primitive likes

--- mod/emacs/list.scm
+++ mod/emacs/list.scm
@@ -20,66 +20,62 @@
 ;;; Basic Cons Cell Operations (Foundation)
 ;;;
 
-(define (elisp-cons car cdr)
-  "Create a new cons, give it CAR and CDR as components, and return it."
-  (cons car cdr))
+(define-elisp-inline (cons a d)
+  "Create a new cons, give it A and D as components, and return it."
+  (cons a d))
 
 ;; info: (elisp) Cons Cells
-(define (elisp-car list)
-  "Return the car of LIST. If LIST is nil, return nil.
-   Error if LIST is not nil and not a cons cell. See also `car-safe'."
+(define-elisp-inline (car lst)
   (cond
-    ((null? list) #nil)
-    ((eq? list #nil) #nil)
-    ((pair? list) (car list))
-    (else (error "Wrong type argument: listp" list))))
+    ((null? lst) #nil)
+    ((eq? lst #nil) #nil)
+    ((pair? lst) (car lst))
+    (else (error "Wrong type argument: listp" lst))))
 
 ;; info: (elisp) Cons Cells
-(define (elisp-cdr list)
-  "Return the cdr of LIST. If LIST is nil, return nil.
-   Error if LIST is not nil and not a cons cell. See also `cdr-safe'."
+(define-elisp-inline (cdr lst)
   (cond
-    ((null? list) #nil)
-    ((eq? list #nil) #nil)
-    ((pair? list) (cdr list))
-    (else (error "Wrong type argument: listp" list))))
+    ((null? lst) #nil)
+    ((eq? lst #nil) #nil)
+    ((pair? lst) (cdr lst))
+    (else (error "Wrong type argument: listp" lst))))
 
-(define (elisp-car-safe object)
+(define-elisp-inline (car-safe object)
   "Return the car of OBJECT if it is a cons cell, or else nil."
   (if (pair? object) (car object) #nil))
 
-(define (elisp-cdr-safe object)
+(define-elisp-inline (cdr-safe object)
   "Return the cdr of OBJECT if it is a cons cell, or else nil."
   (if (pair? object) (cdr object) #nil))
 
-(define (elisp-caar list) (elisp-car (elisp-car list)))
-(define (elisp-cadr list) (elisp-car (elisp-cdr list)))
-(define (elisp-cdar list) (elisp-cdr (elisp-car list)))
-(define (elisp-cddr list) (elisp-cdr (elisp-cdr list)))
+(define-elisp-inline (caar lst) (elisp-car (elisp-car lst)))
+(define-elisp-inline (cadr lst) (elisp-car (elisp-cdr lst)))
+(define-elisp-inline (cdar lst) (elisp-cdr (elisp-car lst)))
+(define-elisp-inline (cddr lst) (elisp-cdr (elisp-cdr lst)))
 
-(define (elisp-list . elms)
+(define-elisp-inline (list . elms)
   elms)
 
-(define (elisp-make-list len obj)
+(define-elisp-inline (make-list len obj)
   (make-list len obj))
 
-(define (elisp-not x)
+(define-elisp-inline (not x)
   (if (nil? x) #t #nil))
 
-(define (elisp-rplaca cell x)
+(define-elisp-inline (rplaca cell x)
   (set-car! cell x))
 
-(define (elisp-rplacd cell x)
+(define-elisp-inline (rplacd cell x)
   (set-cdr! cell x))
 
-(define (elisp-delq elt list)
+(define (elisp-delq elt lst)
   "Delete members of LIST which are `eq' to ELT, and return the result.
 More precisely, this function skips any members `eq' to ELT at the
 front of LIST, then removes members `eq' to ELT from the remaining
 sublist by modifying its list structure, then returns the resulting list."
   ; FIX: could probably use a simple destructive filter:
-  (filter! (lambda (x) (not (eq? elt x))) list)
-  (let skip-front ((tail list))
+  (filter! (lambda (x) (not (eq? elt x))) lst)
+  (let skip-front ((tail lst))
     (cond
       ((or (null? tail) (eq? tail #nil)) #nil)
       ((eq? elt (car tail)) (skip-front (cdr tail)))
@@ -93,62 +89,49 @@ sublist by modifying its list structure, then returns the resulting list."
            (else
             (remove-rest curr (cdr curr)))))))))
 
-(define (elisp-remq elt list)
+(define (elisp-remq elt lst)
   "Return a copy of LIST with all elements `eq' to ELT removed."
-  (let loop ((tail list) (result '()))
+  (let loop ((tail lst) (result '()))
     (cond
       ((null? tail) (reverse result))
       ((eq? elt (car tail)) (loop (cdr tail) result))
       (else (loop (cdr tail) (cons (car tail) result))))))
 
-(define (elisp-assq key alist)
+(define (elisp-assq key alst)
   "Return non-nil if KEY is `eq' to the car of an element of ALIST.
 The value is actually the first element of ALIST whose car is KEY.
 Elements of ALIST that are not conses are ignored."
-  (let loop ((tail alist))
+  (let loop ((tail alst))
     (cond
       ((or (null? tail) (eq? #nil tail)) #nil)
       ((not (pair? (car tail))) (loop (cdr tail))) ; Skip non-conses
       ((eq? key (car (car tail))) (car tail))
       (else (loop (cdr tail))))))
 
-(define (elisp-memq elt list)
+(define-elisp-inline (memq elt lst)
   "Return non-nil if ELT is an element of LIST. Comparison done with `eq'.
 The value is actually the tail of LIST whose car is ELT."
-  (or (memq elt list) #nil))
+  (or (memq elt lst) #nil))
 
-(define (elisp-memql elt list)
+(define-elisp-inline (memql elt lst)
   "Return non-nil if ELT is an element of LIST.  Comparison done with `eql'.
 The value is actually the tail of LIST whose car is ELT."
-  (or (memv elt list) #nil))
+  (or (memv elt lst) #nil))
 
-(define (elisp-member elt list)
+(define-elisp-inline (member elt lst)
   "Return non-nil if ELT is an element of LIST. Comparison done with `equal'.
 The value is actually the tail of LIST whose car is ELT."
-  (or (member elt list) #nil))
+  (or (member elt lst) #nil))
 
 ;;
 
 (define (init-list-registrations)
   "initialize primordial elisp functionality"
+  ;; Register non-inlined primitives
   (for-each (lambda (sym-fun)
+              (format (current-error-port) "-- registering ~s~%" sym-fun)
               (set-symbol-function! (car sym-fun) (cadr sym-fun)))
-            `(
-              (cons ,elisp-cons)
-              (car ,elisp-car)
-              (cdr ,elisp-cdr)
-              ;; (car-safe ,elisp-car-safe)
-              ;; (cdr-safe ,elisp-cdr-safe)
-              (caar ,elisp-caar)
-              (cadr ,elisp-cadr)
-              (cdar ,elisp-cdar)
-              (cddr ,elisp-cddr)
-              (list ,elisp-list)
-              (make-list ,elisp-make-list)
-              (not ,elisp-not)
-              (rplaca ,elisp-rplaca)
-              (rplacd ,elisp-rplacd)
-              (delq ,elisp-delq)
+            `((delq ,elisp-delq)
               (remq ,elisp-remq)
               (assq ,elisp-assq)
               (memq ,elisp-memq)
